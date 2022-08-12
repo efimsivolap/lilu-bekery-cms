e8189cc10fcf44fdcb44ff660c3f192ff375bb81
Efim Sivolap <efim.sivolap@gmail.com>
2022-08-12 10:04:19 +0500

    server express

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require("express");
+
+// создаю экземпляр фреймворка
+const app = express();
---
+++ index.js
@@ -0,0 +1,2 @@
+// запускаю простой сервер
+app.listen(5000, () => console.log("server has been started"));
