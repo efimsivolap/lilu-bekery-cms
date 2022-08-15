68da73a4d923bb00c9a12480ca55f5abfcdf68d8
Efim Sivolap <efim.sivolap@gmail.com>
2022-08-15 11:13:54 +0500

    email password

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const authRoutes = require('./routes/auth')
-const analyticsRoutes = require('./routes/analytics')
-const categoryRoutes = require('./routes/category')
-const orderRoutes = require('./routes/order')
-const positionRoutes = require('./routes/position')
-const keys = require('./config/keys')
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const orderRoutes = require("./routes/order");
+const positionRoutes = require("./routes/position");
+const keys = require("./config/keys");
+const app = express();
+const DB =
+  "mongodb+srv://efim_sivolap:<password>@cluster0.t2z2jvw.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(keys.mongoURI)
-  .then(() => console.log('MongoDB connected.'))
-  .catch(error => console.log(error))
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => console.log("MongoDB connected."))
+  .catch((error) => console.log(error));
 
-app.use(require('morgan')('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(require('cors')())
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require("cors")());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/analytics', analyticsRoutes)
-app.use('/api/category', categoryRoutes)
-app.use('/api/order', orderRoutes)
-app.use('/api/position', positionRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/position", positionRoutes);
 
-
-module.exports = app
+module.exports = app;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,23 @@
-module.exports.login = function(req, res) {
+module.exports.login = function (req, res) {
   res.status(200).json({
     login: {
       email: req.body.email,
-      password: req.body.password
-    }
-  })
-}
+      password: req.body.password,
+    },
+  });
+};
 
-
-module.exports.register = function(req, res) {
-  res.status(200).json({
-    register: 'from controller'
-  })
-}
+module.exports.register = async function (req, res) {
+  const candidate = await URLSearchParams.findOne({
+    // email password
+    email: req.body.email,
+  });
+  if (candidate) {
+    // если пользователь существут то выдаст ошибку
+    res.status(409).json({
+      message: "такой email уже занят.",
+    });
+  } else {
+    // создаём нового пользователя
+  }
+};
